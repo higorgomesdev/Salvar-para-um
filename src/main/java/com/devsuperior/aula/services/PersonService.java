3dd0b7493dce5555647f3be8ea73f7e13fbbf1f8
Higor Gomes <higorddtank123@gmail.com>
2026-04-23 18:23:36 -0300

    POST para um com recebendo classe (PersonDepartmentId)

---
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.aula.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.aula.DTO.PersonDepartmentDTO;
+import com.devsuperior.aula.entities.Department;
+import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.DepartmentRepository;
+import com.devsuperior.aula.repositories.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	private PersonRepository repository;
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
+		
+		Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
+		
+		//Department dept = new Department();
+		//dept.setId(dto.getDepartment().getId());
+		Person entity = new Person(null, dto.getName(), dto.getSalary(), dept);
+		entity = repository.save(entity);
+		
+		return new PersonDepartmentDTO(entity);
+	}
+}
